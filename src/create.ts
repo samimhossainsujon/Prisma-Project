e207f7cae29678b3b40cd733d61960787f0b8dc3
samimhossainsujon <samimhossainsanto@gmail.com>
2024-03-12 20:18:43 +0600

    main done

--- src/create.ts
+++ src/create.ts
@@ -3,6 +3,8 @@ import { PrismaClient, UserRole } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const main = async () => {
+
+  
   //   const createUser = await prisma.user.create({
   //       data: {
   //           username: "user2",
